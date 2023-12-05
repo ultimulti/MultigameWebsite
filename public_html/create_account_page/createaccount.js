0a8a46f12f137a2700a8fe2c41a35e36f2323848
Penny <pj.enterline@gmail.com>
2023-12-05 01:52:21 -0700

    basic acc create page, moved img to folder

---
+++ public_html/create_account_page/createaccount.js
@@ -0,0 +1,12 @@
+
+function create() {
+  // popup for username taken
+
+  // popup for account creation
+
+  window.alert("Account Created!");
+
+
+  window.location.href = "../login_page/login.html"
+}
+
