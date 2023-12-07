f6564f96833891e8cd196338fd4b019dfbd35de2
Penny Enterline <38709645+pennyenterline@users.noreply.github.com>
2023-12-06 23:51:10 -0700

    Update leaderboard.js

--- public_html/leaderboard_page/leaderboard.js
+++ public_html/leaderboard_page/leaderboard.js
@@ -16,7 +16,7 @@ function wordleLB() {
                     <p id="lb_username">${items[i].username}</p>
                     <p id="lb_score">${items[i].wordleHS}</p>
                     </div>`;
-                    if (1 == 9) {
+                    if (count == 9) {
                         break
                     }
             }
@@ -48,7 +48,7 @@ function connect4LB() {
                     <p id="lb_username">${items[i].username}</p>
                     <p id="lb_score">${items[i].connect4HS}</p>
                     </div>`;
-                    if (1 == 9) {
+                    if (count == 9) {
                         break
                     }
             }
